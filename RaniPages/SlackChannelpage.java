bd13b2c7398ef437208e0088b1ee4f4c0359478f
HalidaT <halidan.tuerxun@gmail.com>
2022-02-27 18:00:18 -1000

    commit with Rani's code

---
+++ RaniPages/SlackChannelpage.java
@@ -0,0 +1,32 @@
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utilities.Driver;
+public class SlackChannelpage {
+
+public SlackChannelpage() {
+		
+		PageFactory.initElements(Driver.getDriver(), this);
+	}	
+		@FindBy(id = "username")
+		public WebElement username;
+
+		@FindBy(id = "password")
+		public WebElement password;
+		
+		@FindBy(xpath="//button[@type='submit' and contains(text(),'Sign in')]")
+		public WebElement SigninBtn;
+
+		@FindBy(linkText = "Slack Channel")
+		public WebElement slacktab;
+
+		@FindBy WebElement Slack;
+		
+		@FindBy(xpath = "//input[@id='email']")
+		public WebElement slacksignin;
+
+		
+	}
+
